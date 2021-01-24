5be24e60bfec9c5d788405b2432c80ebea7525be
Thaileaf <lethaison2019@gmail.com>
2021-01-24 00:10:00 -0800

    more websocket learning

--- WebServerSocketPt2.py
+++ WebServerSocketPt2.py
@@ -2,19 +2,32 @@ import io
 import socket
 import sys
 
-address_family = socket.AF_INET
-socket_type = socket.SOCK_STREAM
 
 class WSGIServer:
     address_family = socket.AF_INET
     socket_type = socket.SOCK_STREAM
     request_queue_size = 1
 
-    def __init__(self):
-        self.listen.socket = listen_socket = socket.socket(
+    def __init__(self, server_address):
+        # Initializes the listening socke
+        self.listen_socket = listen_socket = socket.socket(
             self.address_family,
             self.socket_type
         )
 
+
+        # Sets socket option of reusing address at level of sol_socket(socket level) to 1 (True)
         listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listen_socket.bind(address_family)
+        # Binds address to socket
+        listen_socket.bind(server_address)
+        # Listen to (request size) amount of requests
+        listen_socket.listen(self.request_queue_size)
+
+        # Gets server host name and port
+        host, port = self.listen_socket.getsockname()[:2]
+        # Get fully qualified domain name
+        self.server_name = socket.getfqdn(host)
+        self.server_port = port
+
+        self.headers_set = []
+
